a9b3def534c4996ff2982c49f6513debd8882dc7
Janderson Cassan <jande.max@gmail.com>
2020-07-13 19:29:56 -0300

    add encrypt

---
+++ src/main/java/com/br/carro/config/DataBaseConfig.java
@@ -0,0 +1,35 @@
+package com.br.carro.config;
+
+import javax.sql.DataSource;
+
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DataBaseConfig {
+	
+	@Value("${spring.datasource.url}")
+	private String url;
+		
+	@Bean
+    public DataSource dataSource()  {
+       HikariConfig hikariConfig = new HikariConfig();
+       hikariConfig.setJdbcUrl(getData(url));
+       hikariConfig.setConnectionTestQuery("select 1 from dual");
+       hikariConfig.setPoolName("POOL-MYSQL");       
+       
+       return new HikariDataSource(hikariConfig);
+    }
+
+	private String getData(String key) {
+		BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+		textEncryptor.setPassword("heroku");
+		return textEncryptor.decrypt(key);
+	}
+	
+}
